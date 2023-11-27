e4266781ea27e1e54c48308cd48d235cd1ff33a9
Catiier <yerlin.n.r.vivanco@gmail.com>
2023-11-26 23:31:53 -0400

    Sale

---
+++ PomaBrothers/Models/DTOModels/SaleDetailDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Net.Sockets;
+
+namespace PomaBrothers.Models.DTOModels
+{
+    public class SaleDetailDTO
+    {
+        public int EmployeeId { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public decimal Total { get; set; }
+
+        public DateTime RegisterDate { get; set; }
+
+        public List<ProductSaledDTO> ProductSaled { get; set; }
+    }
+}
